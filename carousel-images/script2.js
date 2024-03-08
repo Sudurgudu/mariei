9baa0beef39eac9e1d72f678fe5035063f176697
Sudurgudu <Sudurgudu@gmail.com>
2024-03-08 11:11:54 -0500

    changeded

--- carousel-images/script2.js
+++ carousel-images/script2.js
@@ -4,50 +4,50 @@ function cliked(a){
     localStorage.setItem("Name","Joel Corry");
     localStorage.setItem("Date", "3/7/2024");
     localStorage.setItem("index","1");
-    window.location.href="cart/index.html"
+    window.location.href="cart/"
 }
 
 if(a===2){
     localStorage.setItem("Name","Tom Macari");
     localStorage.setItem("Date", "3/8/2024");
     localStorage.setItem("index","2");
-    window.location.href="cart/index.html"
+    window.location.href="cart/"
 }
 if(a===3){
     localStorage.setItem("Name","Jesse Marco");
     localStorage.setItem("Date", "3/9/2024");
     localStorage.setItem("index","3");
-    window.location.href="cart/index.html"
+    window.location.href="cart/"
 }
 if(a===4){
     localStorage.setItem("Name","Jason Smith");
     localStorage.setItem("Date", "3/14/2024");
     localStorage.setItem("index","4");
-    window.location.href="cart/index.html"
+    window.location.href="cart/"
 }
 if(a===5){
     localStorage.setItem("Name","Pisano");
     localStorage.setItem("Date", "3/15/2024");
     localStorage.setItem("index","5");
-    window.location.href="cart/index.html"
+    window.location.href="cart/"
 }
 if(a===6){
     localStorage.setItem("Name","Tay James");
     localStorage.setItem("Date", "3/16/2024");
     localStorage.setItem("index","6");
-    window.location.href="cart/index.html"
+    window.location.href="cart/"
 }
 if(a===7){
     localStorage.setItem("Name","Marty Rock");
     localStorage.setItem("Date", "3/21/2024");
     localStorage.setItem("index","7");
-    window.location.href="cart/index.html"
+    window.location.href="cart/"
 }
 if(a===8){
     localStorage.setItem("Name","DJ Icon");
     localStorage.setItem("Date", "3/22/2024");
     localStorage.setItem("index","8");
-    window.location.href="cart/index.html"
+    window.location.href="cart/"
 }
 
 
